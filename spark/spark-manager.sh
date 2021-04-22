cae0eb557fc3736ce9a3d98a1bf3738039743100
Giovanni Borgh <gio96b@gmail.com>
2021-04-23 01:36:50 +0200

    Cleaning Dockerfile and sh script

--- spark/spark-manager.sh
+++ spark/spark-manager.sh
@@ -5,26 +5,6 @@
 
 echo "Running action ${SPARK_ACTION}"
 case ${SPARK_ACTION} in
-"example")
-echo "Running example ARGS $@"
-./bin/run-example $@
-;;
-"spark-shell")
-./bin/spark-shell --master local[2]
-;;
-"pyspark")
-./bin/pyspark --master local[2]
-;;
-"spark-submit-python")
- ./bin/spark-submit --packages $2 /opt/tap/$1
-;;
-"spark-submit-apps")
- ./bin/spark-submit --packages $3 --class $1 /opt/tap/apps/$2
-;;
-"pytap")
-cd /opt/tap/
-python ${TAP_CODE}
-;;
 "wordcount")
 python3 /opt/tap/wordcount.py ${TAP_CODE}
 ;;
